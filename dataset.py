92345193c31af2a0f6742ea0d308fc1c8203b2c9
Siddharth Tripathi <tripathisiddharth765@gmail.com>
2026-07-08 22:29:59 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -30,6 +30,15 @@ def get_dataloaders():
     )
 
     return train_loader, test_loader
+    
+test_transformer=transforms.Compose([
+    transforms.Resize(size=(224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], 
+        std=[0.229, 0.224, 0.225]
+    )
+])    
 classes = [
     "Cardboard",
     "Glass",
