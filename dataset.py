278250685ff81b84bbf2c7be9b3fc26ac4ad5414
Siddharth Tripathi <tripathisiddharth765@gmail.com>
2026-07-08 22:22:00 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -6,36 +6,38 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-train_transformer = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.ColorJitter(
-        brightness=0.2,
-        contrast=0.2,
-        saturation=0.2
-    ),
-    transforms.Resize(size=(224,224)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485,0.456,0.406],
-        std=[0.229,0.224,0.225]
+def get_dataloaders():
+    train_dataset = datasets.ImageFolder(
+        train_path,
+        transform=train_transformer
     )
-])
-test_transformer=transforms.Compose([
-    transforms.Resize(size=(224,224)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406], 
-        std=[0.229, 0.224, 0.225]
+
+    test_dataset = datasets.ImageFolder(
+        test_path,
+        transform=test_transformer
     )
-])
 
-train_dataset=datasets.ImageFolder(train_path,transform=train_transformer)
-test_dataset=datasets.ImageFolder(test_path,transform=test_transformer)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=32,
+        shuffle=True
+    )
 
-train_loader=DataLoader(dataset=train_dataset,batch_size=32,shuffle=True)
-test_loader=DataLoader(dataset=test_dataset,batch_size=32,shuffle=False)
-classes=train_dataset.classes
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=32,
+        shuffle=False
+    )
+
+    return train_loader, test_loader
+classes = [
+    "Cardboard",
+    "Glass",
+    "Metal",
+    "Paper",
+    "Plastic",
+    "Trash"
+]
 
 
 
